bc6dac3ffae21c90200e3232a82d5a24360a4e34
aabidshaikh86 <69650449+aabidshaikh86@users.noreply.github.com>
2020-12-09 19:54:56 +0530

    Add files via upload

---
+++ Day2 B32.py
@@ -0,0 +1,211 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Introduction to variables in python --- This is used for Comment
+
+
+# In[1]:
+
+
+x = 5
+y = 3 
+
+
+# In[2]:
+
+
+x+y          
+
+
+# In[ ]:
+
+
+Rules for declaring the Variable in python.
+
+
+# In[ ]:
+
+
+Rule No 1: No Namespace while declaring the variable name
+        
+
+
+# In[5]:
+
+
+firstname = 'Abid'
+print(firstname)
+
+
+# In[6]:
+
+
+first_name = 'sheikh'
+print(first_name)
+
+
+# In[ ]:
+
+
+Rule No 2 : you should not start a variable name with a number
+
+
+# In[7]:
+
+
+1name = 'Abid'
+print(1name)
+
+
+# In[8]:
+
+
+name1 ='abid sheikh'
+print (name1)
+
+
+# In[ ]:
+
+
+Role No 3: variable name should not include any Special Characters
+
+
+# In[9]:
+
+
+name@ = 'Abid'
+print(name@)
+
+
+# In[ ]:
+
+
+Interview Question : What are the rules that we need to be aware of for declaring an variable in python.
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Introduction to Datatypes in Python:
+    
+    
+    1. string................> str
+    2. numbers ..............> integers (int) and floats (decimals)
+    3. list .................> list
+    4. tuple ................> tuple
+    5. dictionaries..........> dict
+
+
+# In[ ]:
+
+
+Classification of datatypes:
+      
+        Two different categories:
+            
+            1. mutable datatypes
+            2. immutable datatypes
+              
+Mutable Datatypes : Which we can able to edit or alter after the datatypes has been declared
+Immutable         : Which we cannot edit or alter..    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Introduction to string datatype:
+
+
+# In[ ]:
+
+
+Defination : A string is a series of characters declared in quotes.
+
+Classification : it has been classifies as an immutable datatype.
+    
+Declaretion : '' , " " , "" "" , 3 possible ways
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+name = 'Hi Abid'
+print(name)
+
+
+# In[12]:
+
+
+name2 = "Hi Abid"
+print(name2)
+
+
+# In[13]:
+
+
+name3 = """Hi Abid"""
+print(name3)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+Introduction to String Methods:
+
+
+# In[14]:
+
+
+type(name)
+
+
+# In[15]:
+
+
+type(name2)
+
+
+# In[16]:
+
+
+type(name3)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
