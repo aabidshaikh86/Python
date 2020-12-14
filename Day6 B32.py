2d1b2e8a3cc81d759b5d012e8d5ce3cf6f31c85e
aabidshaikh86 <69650449+aabidshaikh86@users.noreply.github.com>
2020-12-14 23:10:12 +0530

    Add files via upload

---
+++ Day6 B32.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+continuation to lists::
+
+
+# In[ ]:
+
+
+
+
+
+# In[2]:
+
+
+students = ['kiran','ravi','firoz','mohini','vaani','swapna']
+
+
+# In[ ]:
+
+
+Req : I want to appreciate them for complementing the task on time and sending me the practice files.
+
+
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+x = f"keep up the good work, {students[0].title()}"
+
+
+# In[11]:
+
+
+print(x)
+
+
+# In[12]:
+
+
+y = f"keep up the good work, {students[1].title()}"
+
+
+# In[14]:
+
+
+print(y)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+**Introduction to for looping**
+
+----- FORMULA ----
+
+for tempvar in mainvar:    ## tempvar will be anytime
+    print(tempvar)         ## mainvar will be name or value
+# In[ ]:
+
+
+
+
+
+# In[39]:
+
+
+for x in students:
+    print(f'keep up the goodwork, {x.title()}')
+
+
+# In[ ]:
+
+
+
+
+
+# In[22]:
+
+
+for test in students:
+    print(test.upper())
+
+
+# In[40]:
+
+
+for superman in students:
+    print(superman.lower())
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+## Enhancment of the code ...!! I also want them to send me the parctise files of todays..!!
+
+
+# In[24]:
+
+
+for x in students:
+    print(f'keep up the goodwork, {x.title()}')
+    print(f'send me the practices files of today,{x.title()}')    ## indentation.
+
+
+# In[27]:
+
+
+for x in students:
+    print(f'keep up the goodwork, {x.title()}')
+    print()
+    print(f'send me the practices files of today,{x.title()}')
+
+
+# In[ ]:
+
+
+
+
+for x in students:
+print(x)       ### Error indentation (out of Loop) ###
+# In[ ]:
+
+
+## Zen of python
+
+
+# In[29]:
+
+
+import this
+
+
+# In[ ]:
+
+
+
+
